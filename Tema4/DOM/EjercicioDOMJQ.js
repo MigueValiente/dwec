a0aa19cdbbfb7575fcabf53cfc54fe9e78943944
Miguel Angel Valiente <miguevaliente96@gmail.com>
2018-11-26 21:18:36 +0100

    ejercicios JQuery

--- Tema4/DOM/EjercicioDOMJQ.js
+++ Tema4/DOM/EjercicioDOMJQ.js
@@ -1,46 +1,100 @@
-// 1.Crear un formulario
-// 2.Crear 10 div dentro del formulario
-// 3.Cada div tiene el mismo numero de inputs que su posicion
-// 4.Cada input en posicion par es de color rojo y el impar verde
-// 5.Cada input un id distinto y el valor representa su posicion
-// 6.Crear un boton dentro del formulario 
-
 function crearForm(numeroDiv){
-    //Crea el formulario
-   let form = document.createElement("form");
-   $(form);
-   $(form).id = "form1";
-   $(form).name = "formulario";
-   $(form).method = "POST";
+    let body = $(document.body);
     //Crea el numero de div introducido
-   for (let i = 1; i <= numeroDiv; i++) {
-       let d = document.createElement("div");
+    for (let i = 1; i <= numeroDiv; i++) {
+        let div = $("<div></div>");
+        let h1 = $("<h1></h1>");
+        h1.text(`Div nº${i}`);
+        div.append(h1);
+        $(document.body).append(div);
+    }
        //Crear los input
-       for (let j = 1; j <= i; j++) {
-           let input = document.createElement("input");
-           input.type = "text";
-           input.id = `${i}.${j}`;
-           input.value = `${i}.${j}`;
-           input.autocomplete = "off";
-           input.setAttribute("onmouseover","cambiarRosa('"+input.id+"')");
-           if(j%2 === 0){
-               input.className = "par";
-           }else{
-               input.className = "impar";
-           }
-           d.appendChild(input);
-       }
-       form.appendChild(d);
+    let divs = $("body div");
+    for (let i = 1; i <= divs.length; i++) {
+        for (let j = 1; j <= i; j++) {
+            let input = $("<input />");
+            input.attr("id",`${i}_${j}`);
+            input.attr("disabled","true");
+            input.attr("value",`${i}.${j}`);
+            if(j%2 === 0){
+                input.addClass("par");
+            }else{
+                input.addClass("impar");
+            }
+            $(divs[i-1]).append(input)
+            
+        }
    }
-   let boton = document.createElement("input");
-   boton.type = "button";
-   boton.name = "boton";
-   boton.setAttribute("onclick","cambiarColores()");
-   boton.id = "boton";
-   boton.value = "Cambiar colores";
-   form.appendChild(boton);
-   document.body.appendChild(form);
+   let botonP = $("<button></button>");
+   botonP.text("Eliminar Pares");
+   botonP.click(function(event){
+    let pares = $("body .par");
+        for (let i = 0; i < pares.length; i++) {
+            pares[i].remove(); 
+        }
+   });
+   $(document.body).append(botonP);
+
+   let botonI = $("<button></button>");
+   botonI.text("Eliminar Inputs");
+   botonI.attr("data-div","7");
+   botonI.click(function(event){
+        borrarInputs($(event.target).attr("data-div"));
+   });
+   $(document.body).append(botonI);
+
+   let texto = $("<input />");
+   texto.attr("id","posicionInput");
+   texto.attr("type", "text");
+
+   let botonCambiar = $("<button></button>");
+   botonCambiar.text("Cambiar Hermanos");
+   botonCambiar.click(function(event){
+    let posicion = $("#posicionInput").val().trim();
+    let input = $(`input[value='${posicion}']`)
+    let hIzquierda = input.prev("input");
+    let hDerecha = input.next("input");
+    if(input.hasClass("impar")){
+        hIzquierda.removeClass("par");
+        hDerecha.removeClass("par")
+        hIzquierda.addClass("impar");
+        hDerecha.addClass("impar")
+    }else{
+        hIzquierda.removeClass("impar");
+        hDerecha.removeClass("impar")
+        hIzquierda.addClass("par");
+        hDerecha.addClass("par")
+    }
+   });
+
+
+   $(document.body).append(texto,botonCambiar);
 }
+
+function borrarInputs(posicion){
+    let div = $("body div").eq(posicion);
+    div.find("input.impar").remove();
+}
+
+function cambiarClases(){
+    let posicion = $("#posicionInput").val().trim();
+    let input = $("body div").find(posicion);
+    let hIzquierda = input.prev();
+    let hDerecha = input.next();
+    if(hIzquierda.hasClass("impar")){
+        hIzquierda.removeClass("impar");
+        hIzquierda.addClass("par");
+        hDerecha.removeClass("par");
+        hDerecha.addClass("impar");
+    }else{
+        hIzquierda.removeClass("par");
+        hIzquierda.addClass("impar");
+        hDerecha.removeClass("impar");
+        hDerecha.addClass("par");
+    }
+}
+
+
 
 function cambiarColores(){
     let inputs = document.body.getElementsByTagName("input");
@@ -58,8 +112,13 @@ function cambiarRosa(id){
     input.style.backgroundColor = "pink";
 
 }
-    
 
-crearForm(10);
+$(document).ready(
+    function(){
+        crearForm(10);
+    }
+);
+
+
 
 
