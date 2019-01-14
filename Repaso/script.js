077f8d3f965298daef27e1f3988e30d80a798bb9
Miguel Angel Valiente <miguevaliente96@gmail.com>
2019-01-14 21:02:36 +0100

    Ejercicio Equipos de Futbol

--- Repaso/script.js
+++ Repaso/script.js
@@ -16,9 +16,59 @@ function crearInput(){
         
         let valor = event.target.value;
         valor = valor.trim();
-
-        aplicarFiltro(valor);
+        
+        // aplicarFiltro(valor);
+        validarInput(event.target,valor);
     });
+}
+
+function validarInput(input,valor){
+    let expresion1 = /^[A-Za-z]+$/;
+    let expresion2 = /^[A-Z]+$/;
+    
+    let esCorrectoTodoLetras = expresion1.test(valor);
+    let esCorrectoTodoMayusculas = expresion2.test(valor);
+    let esCorrectoTamano = valor.length >= 5;
+
+    let divConErrores = document.getElementById("divErroresInput");
+    if(divConErrores === null){
+        divConErrores = document.createElement('div');
+        divConErrores.id = "divErroresInput";
+    }else{
+        divConErrores.innerHTML = "";
+    }
+
+   input.parentNode.insertBefore(divConErrores,input.nextSibling);
+
+    if(!esCorrectoTodoLetras){
+        let span = document.createElement('span');
+        span.style.color = "red";
+        span.innerHTML = "Solo se permiten letras.<br>";
+        divConErrores.appendChild(span);
+    }
+
+    if(!esCorrectoTodoMayusculas){
+        let span = document.createElement('span');
+        span.style.color = "red";
+        span.innerHTML = "Solo se permiten letras Mayusculas.<br>";
+        divConErrores.appendChild(span);
+    }
+
+    if(!esCorrectoTamano){
+        let span = document.createElement('span');
+        span.style.color = "red";
+        span.innerHTML = "Debe tener más de 5 caracteres.<br>";
+        divConErrores.appendChild(span);
+    }
+
+   input.style.border = "";
+
+    if(divConErrores.innerHTML === ""){
+       input.style.border = "solid green 1px";
+       aplicarFiltro(valor);
+    }else{
+       input.style.border = "solid red 1px";
+    }
 }
 
 function mostrarLigas(){
@@ -27,18 +77,16 @@ function mostrarLigas(){
     contenedor.innerHTML += Liga2.toString();
 }
 
+function mostrarEquipo(equipo,div, valor){
+    if(equipo.nombre.toUpperCase().includes(valor.toUpperCase())){
+        div.innerHTML += equipo.toString();
+    }
+}
+
 function aplicarFiltro(valor){
     let contenedor = document.getElementById("contendorResultados");
-        contenedor.innerHTML = "";
-        
-    for (let i = 0; i < arrayLigas.length; i++) {
-        for (let j = 0; j < arrayLigas[i].equipos.length; j++) {
-            if(arrayLigas[i].equipos[j].nombre.includes(valor)){
-                contenedor.innerHTML += arrayEquipos[i].toString();
-            } 
-            
-        }
-        
-    }
+    contenedor.innerHTML = "";
+
+    arrayLigas.forEach(liga => liga.equipos.forEach(equipo => mostrarEquipo(equipo,contenedor,valor)))
 }
 
